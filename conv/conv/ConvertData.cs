122d7519f6598e4ab473212baffd30c2b90b5679
unknown <@ngknn.local>
2024-04-05 16:46:20 +0300

    create struct models and rules get path

--- conv/conv/ConvertData.cs
+++ conv/conv/ConvertData.cs
@@ -6,7 +6,139 @@ using System.Threading.Tasks;
 
 namespace conv
 {
+    struct Place
+    {
+        public string country;
+        public string city;
+        public string street;
+        public string house;
+    }
+
+    struct Programm
+    {
+        public string title;
+        public string versoin;
+        public string dataReilese;
+    }
+
     internal class ConvertData
     {
+        //путь чтения
+        string pathRead;
+        //путь записи
+        string pathWrite;
+        //переменная для обработки ошибок
+        bool flag;
+
+        //списки структур
+        List<Place> places;
+        List<Programm> programms;
+
+
+        //получение пути считывания
+        public string PathRead
+        {
+            set
+            {
+                if (CheckFormats(value) && CheckingForTheExistenceOfAFile(value))
+                {
+                    pathRead = value;
+                    flag = true;
+                }
+                else
+                {
+                    Console.WriteLine("Файл не существует, либо путь к файлу указан не верно!");
+                    pathRead = "";
+                }
+            }
+        }
+
+        //получение пути записи
+        public string PathWrite
+        {
+            set
+            {
+                if (CheckFormats(value))
+                {
+                    pathRead = value;
+                    flag = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// проверка поступившей строки с путём к файлу на формат самого файла
+        /// </summary>
+        /// <param name="str">строка</param>
+        /// <returns>
+        /// true - верный формат
+        /// false - фармат не верный (его нет)
+        /// </returns>
+        bool CheckFormats(string str)
+        {
+            if (str.Length < 4)
+            {
+                return false;
+            }
+            return pathRead.IndexOf(".json") != -1
+                || pathRead.IndexOf(".xml") != -1
+                || pathRead.IndexOf(".yaml") != -1
+                || pathRead.IndexOf(".csv") != -1;
+        }
+
+        /// <summary>
+        /// проверка на существование файла по заданному пути
+        /// </summary>
+        /// <param name="str">путь</param>
+        /// <returns>
+        /// true - файл существует
+        /// false - файл не существует
+        /// </returns>
+        bool CheckingForTheExistenceOfAFile(string str)
+        {
+            return File.Exists(str);
+        }
+
+
+        /// <summary>
+        /// Полчение расширения файла без точки
+        /// </summary>
+        /// <param name="str">путь к файлу (полный или относительный) с названием и расширением файла</param>
+        /// <returns>
+        /// строка - расширение файла без точки
+        /// </returns>
+        string GetFormat(string str)
+        {
+            string revStr = str.ToCharArray().Reverse().ToString();
+            return revStr.Substring(0, revStr.IndexOf('.'));
+        }
+
+        public void ReadFile()
+        {
+            if (flag)
+            {
+                using (StreamReader file = new StreamReader(pathRead))
+                {
+
+                }
+            }
+            else
+                return;
+        }
+
+        public void WriteFile()
+        {
+            if (flag)
+            {
+                using (StreamWriter file = new StreamWriter(pathRead, false))
+                {
+
+                }
+            }
+            else
+                return;
+        }
+
+
     }
 }
--- conv/conv/Program.cs
+++ conv/conv/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать!\nВыберите вариант действия (введите номер в виде числа):\n1.Считать модель\n2.Записать модель\n3.Выйти");
+            Console.Write("Ваш выбор: ");
+            int variantDo = int.Parse(Console.ReadLine());
+
+            ConvertData conv = new ConvertData();
+
+            Console.Clear();
+            switch (variantDo)
+            {
+                case 1:
+                    Console.WriteLine("Вы выбрали сичтывание модели  с файла");
+                    Console.WriteLine("Правила записи пути к файлу:");
+                    Console.WriteLine("1.Вводите путь (полный или относительный) файлу без кавычек и через 2 слеша разделения пути\n2. У файла должно быть чётко указано расширение!\n3.При введения просто названия файла (с расширением), файл создастся по стандартному относительному пути");
+                    Console.Write("Данные файла:\t");
+                    conv.PathRead = Console.ReadLine();
+                    conv.ReadFile();
+                    break;
+                case 2:
+                    Console.WriteLine("Вы выбрали запись модели в файл\n");
+                    Console.WriteLine("Правила записи пути к файлу:");
+                    Console.WriteLine("1.Вводите путь (полный или относительный) файлу без кавычек и через 2 слеша разделения пути\n2. У файла должно быть чётко указано расширение!\n3.При введения просто названия файла (с расширением), файл создастся по стандартному относительному пути");
+                    Console.Write("\nДанные файла:\t");
+                    conv.PathWrite = Console.ReadLine();
+                    conv.WriteFile();
+                    break;
+                default:
+                    Console.WriteLine("До свидания!");
+                    return;
+            }
         }
     }
 }
